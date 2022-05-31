45b9e93977c3dd2e57f02454c63de1ed0f89df55
Tyler Williams <tgwilliams87@gmail.com>
2022-05-31 11:32:32 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let pokemonList = [
-  {name: `bulbasaur`, height: `0.7m`, types: [`grass`, `poison`]},
-  {name: `squirtle`, height: `0.5m`, type: `water`},
-  {name: `charmander`, height: `0.6m`, type: `fire`}
-];
