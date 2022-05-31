599ff453ccc1755ca375375ef31087089bafe128
Tyler Williams <tgwilliams87@gmail.com>
2022-05-31 11:29:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: `bulbasaur`, height: `0.7m`, types: [`grass`, `poison`]},
+  {name: `squirtle`, height: `0.5m`, type: `water`},
+  {name: `charmander`, height: `0.6m`, type: `fire`}
+];
