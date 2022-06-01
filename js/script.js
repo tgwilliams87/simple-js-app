ad32f52cfc6e77577da35dd8be2901128868bcd4
Tyler Williams <tgwilliams87@gmail.com>
2022-06-01 12:53:17 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-  {name: `bulbasaur`, height: `0.7m`, types: [`grass`, `poison`]},
-  {name: `squirtle`, height: `0.5m`, type: `water`},
-  {name: `charmander`, height: `0.6m`, type: `fire`}
+  {name: `Venusaur`, height: `2.0`, types: [`grass`, `poison`]},
+  {name: `Squirtle`, height: `0.4`, type: `water`},
+  {name: `Charmander`, height: `0.6`, type: `fire`}
 ];
+
+for (let i=0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name +' '+'height is '+ pokemonList[i].height +' cm')
+
+    if(pokemonList[i].height >= 1.6){
+        document.write('  Size Large "Wow, thats\'s big!"' + '<br>')
+    } else if(pokemonList[i].height >=0.5 && pokemonList[i].height <=1.0){
+        document.write('  Size Medium' +'<br>')
+    } else {
+        document.write('  Size Small' +'<br>') 
+    }
+}
