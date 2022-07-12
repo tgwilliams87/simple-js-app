868dbdc083965bba4e574f66a69ef8f964be5779
Tyler Williams <tgwilliams87@gmail.com>
2022-07-12 11:30:23 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,52 @@
 let pokemonRepository = (function () {
  let pokemonList = [];
  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+ let modalContainer = document.querySelector('#modal-container'); 
+  
+  function showModal(title,text) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+ let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
 
 
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+  modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal();
+  });
+    
  function add (pokemon) {
     if (
       typeof pokemon === "object" &&
@@ -30,9 +74,9 @@ let pokemonRepository = (function () {
     });
   }
 
-  function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      console.log(item)
+  function showDetails(pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function () {
+       modalContainer.innerHTML = '';
     });    
   }
 
